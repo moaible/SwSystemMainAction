1c3fe31cd42119c9c98deae8982a6f4103894141
moaible <motodera@moapp.info>
2017-01-17 11:17:32 +0900

    Fix access control and async executable

--- Sources/SwSystemMainAction.swift
+++ Sources/SwSystemMainAction.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 
-typealias SystemAction = () -> Void
+public typealias SystemAction = () -> Void
 fileprivate var fulfillAction: SystemAction = {
     exit(EXIT_SUCCESS)
 }
@@ -17,14 +17,14 @@ fileprivate var rejectAction: SystemAction = {
     exit(EXIT_FAILURE)
 }
 
-typealias SystemMainAction = (
+public typealias SystemMainAction = (
     _ path: String,
     _ args: [String],
     _ env: [String: String],
-    _ fulfill: SystemAction,
-    _ reject: SystemAction) -> Void
+    _ fulfill: @escaping SystemAction,
+    _ reject: @escaping SystemAction) -> Void
 
-func systemMain(action: SystemMainAction) {
+public func systemMain(action: @escaping SystemMainAction) {
     action(
         ProcessInfo.processInfo.arguments.first ?? "",
         Array(ProcessInfo.processInfo.arguments.dropFirst()),
@@ -33,4 +33,3 @@ func systemMain(action: SystemMainAction) {
         rejectAction)
     RunLoop.main.run()
 }
-
